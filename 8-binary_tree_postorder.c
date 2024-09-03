ba53f1a2fdfbacfca6a3d2030dc23642ab8ceda8
thehackingdev <nathanavaaga@gmail.com>
2024-09-03 08:18:14 +0300

    binary trees

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,17 +1,13 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - goes through a binary
- * tree using post-order traversal
- * @tree: pointer to the root node of the tree to traverse
- * @func:  pointer to a function to call for each node.
- * The value in the node must be passed as a parameter to this function.
- *
- * Return: No Return
+ * binary_tree_postorder - goes through a binary tree using post-order traverse
+ * @tree: tree to traverse
+ * @func: pointer to a function to call for each node
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (!(tree) || !(func))
+	if (!tree || !func)
 		return;
 
 	binary_tree_postorder(tree->left, func);
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -2,9 +2,10 @@
 
 /**
  * binary_tree_height - measures the height of a binary tree
- * @tree: pointer to the root node of the tree to measure the height
+ * @tree: tree to measure the height of
  *
- * Return: Height or 0 if tree is NULL
+ * Return: height of the tree
+ *         0 if tree is NULL
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
@@ -14,13 +15,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	if (tree->left)
-		height_l = 1 + binary_tree_height(tree->left);
-
-	if (tree->right)
-		height_r = 1 + binary_tree_height(tree->right);
-
-	if (height_l > height_r)
-		return (height_l);
-	return (height_r);
+	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (height_l > height_r ? height_l : height_r);
 }
